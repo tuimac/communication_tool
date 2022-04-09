5ebaddd02f2821461849c2d827b0614af8987a62
tuimac <tuimac.devadm01@gmail.com>
2022-04-09 06:33:47 +0000

    mattermost is done

--- mattermost/run.sh
+++ mattermost/run.sh
@@ -5,9 +5,9 @@ function createContainers(){
     mkdir -p postgresql/{data,logs,wal_archive}
     mkdir -p mattermost/{config,data,logs,plugins,client/plugins,bleve-indexes}
     mkdir -p nginx/{logs}
-    sudo chown 999:999 -R postgresql/
+    sudo chown -R 999:999 postgresql/
     sudo chown -R 2000:2000 mattermost/
-    sudo chown 101:101 -R nginx/
+    sudo chown -R 101:101 nginx/
     docker-compose up -d
 }
 
@@ -15,7 +15,7 @@ function deleteAll(){
     docker-compose down
     sudo rm -rf postgresql/{data,logs,wal_archive}
     sudo rm -rf mattermost
-    sudo rm -rf nginx/{logs}
+    sudo rm -rf nginx/logs
     sudo chown -R ec2-user:ec2-user .
     rm .env
 }
