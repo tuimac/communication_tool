78ff02d8ce77ab9c9d99113d60da3dcc92b28bfa
tuimac <tuimac.devadm01@gmail.com>
2022-04-09 08:10:16 +0000

    save

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,1 +1,35 @@
-#
+#!/bin/bash
+
+DOMAIN='comm.tuimac.com'
+
+function createCerts(){
+    
+}
+
+function userguide(){
+    echo -e "usage: ./letsencrypt.sh.sh [help | create | delete]"
+    echo -e "
+optional arguments:
+create              Create Let's Encrypt certifications.
+delete              Delete  Let's Encrypt certifications.
+    "
+}
+
+function main(){
+    [[ -z $1 ]] && { userguide; exit 1; }
+    case $1 in
+        'create')
+            createCerts
+            ;;
+        'help')
+            userguide
+            exit 1
+            ;;
+        *)
+            userguide
+            exit 1
+            ;;
+    esac    
+}
+
+main
